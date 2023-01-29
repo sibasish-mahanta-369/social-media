a4ec930a8c63621790ce2b529bd756886b6a4ba3
sibasish-mahanta-369 <sibasishmahanta27032001@gmail.com>
2023-01-29 23:17:51 +0530

    updated model

--- models/users.js
+++ models/users.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    username:String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
 })
 
 module.exports = mongoose.model('user',userSchema)
