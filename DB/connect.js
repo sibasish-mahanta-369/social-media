b3b72809ef0942ae8fb7d55bd09c60672ccbcb7f
sibasish-mahanta-369 <sibasishmahanta27032001@gmail.com>
2023-01-29 19:35:05 +0530

    connected to DB and some basic setups

--- DB/connect.js
+++ DB/connect.js
@@ -6,7 +6,7 @@ const connectionString = 'mongodb+srv://Sibasish:1234@cluster0.wgevnrf.mongodb.n
 mongoose.set('strictQuery', true)
 
 const connectDB = (url) =>{
-    mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true})
+    mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology: true})
 }
 
 module.exports = connectDB
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./DB/connect')
+require('dotenv').config()
 
 app.use(express.json())
 
@@ -9,7 +10,7 @@ const port = 3000
 
 const startServer = async() => {
     try {
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         console.log('Connected to MongoDB using mongoose !')
         app.listen(port,()=>{
             console.log(`Server is listenting on port ${port}...`)
